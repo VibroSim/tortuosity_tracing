ee2917805b39f1bd7cd7e85f13193a040f7eaece
Chevonne McInnis <cmcinnis@iastate.edu>
2019-10-16 10:47:48 -0500

    First implimentation of command line prompt that creates a histogram of point-click spacings (needs fine tuning)

---
+++ tortuosity_tracing/bin/point_spacing_calc.py
@@ -0,0 +1,46 @@
+import sys
+import os
+
+import numpy as np
+from matplotlib import pyplot as pl
+
+
+from ..svg_to_histogram import histogram_from_svgs,tortuosity_plots
+
+
+def main(args=None):
+    if args is None:
+        args=sys.argv
+        pass
+        
+    if len(args) < 3:
+        print("Usage: point_spacing_calc <fcutoff_in_1/m> <svg_file.svg> ...")
+        print("Creates tortuosity histograms from the given .svg files.")
+        print("point_spacing is hard wired to 0.1 micron")
+        sys.exit(0)
+        pass
+        
+    fcutoff=float(args[1])
+    svg_files=args[2:]
+    point_spacing=0.1e-6
+    
+
+    (theta_final,
+     thlength_final,
+     filtered_theta_final,
+     eq_lengths_final,
+     mu,
+     mu_F,
+     sigma,
+     sigma_F,
+     tortuosity_path_filenames) = histogram_from_svgs(svg_files,None,fcutoff,None,point_spacing)
+
+    pl.figure()
+    pl.clf()
+    (N,B,P)=pl.hist(thlength_final,bins=50)
+    pl.title('Point Click Spacing',Fontsize=30)
+    pl.xlabel('Lengths [m]',Fontsize=20)
+
+
+    pl.show()
+    pass
