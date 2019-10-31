6d8627479a020f5185ecdc06be0d7761179e98fa
Chevonne McInnis <cmcinnis@iastate.edu>
2019-10-31 16:02:02 -0500

    Added summed crack lengths and a statement telling users to compare these lengths with the specimen database.

--- tortuosity_tracing/pt_steps/tortuosity_tracing_Histogram.py
+++ tortuosity_tracing/pt_steps/tortuosity_tracing_Histogram.py
@@ -42,6 +42,8 @@ def run(_xmldoc,_element,fcutoff_numericunits,point_spacing_numericunits=dc_valu
      tortuosity_path_filenames) = tortuosity_tracing.histogram_from_svgs(svg_filenames,svg_measnums,fcutoff,dest_href.getpath(),point_spacing_numericunits.value(units="m"))
     
     assert(len(tortuosity_path_filenames)==len(svg_filenames))
+    summed_clicked_length=np.sum(thlength_final)
+    summed_eq_length=np.sum(eq_lengths_final)
 
     (unfiltered_filename,filtered_filename) = tortuosity_tracing.tortuosity_plots(
 	tortuosity_path_filenames,
@@ -60,8 +62,13 @@ def run(_xmldoc,_element,fcutoff_numericunits,point_spacing_numericunits=dc_valu
              ("dc:filtered_sigma", dc_value.numericunitsvalue(sigma_F,"radians")),
              ("dc:unfiltered_plot", unfiltered_href),
              ("dc:filtered_plot", filtered_href),
+             ("dc:Full_clicked_length",dc_value.numericunitsvalue(summed_clicked_length,"meters"),
+             ("dc:Full_eq_length",dc_value.numericunitsvalue(summed_eq_length,"meters")
         ]
 
+    print("The length of the crack path based on the point clicks is {} m".format(summed_clicked_length))
+    print("The length of the crack path based on the evenly spaced points is {} m".format(summed_eq_length))
+    print("Check that these to numbers are consitent with each other and greater than the crack length in the specimen database.")
     for filecnt  in range(len(tortuosity_path_filenames)):
         tortuosity_path_filename = tortuosity_path_filenames[filecnt]
         retval.append((("dc:path_comparison", { "measnum" : str(svg_measnums[filecnt]) }), dc_value.hrefvalue(tortuosity_path_filename,contexthref=dest_href)))
